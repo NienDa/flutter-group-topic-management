ca994ff74504e5d7d5acb3d4052db2513796afb6
Nien <huynhthinhaz8@gmail.com>
2026-05-30 22:57:08 +0700

    doan2

--- Flutter_doan/lib/cau_hinh/supabase_cau_hinh.dart
+++ Flutter_doan/lib/cau_hinh/supabase_cau_hinh.dart
@@ -1,15 +1,11 @@
 class SupabaseCauHinh {
-  /// URL của dự án Supabase.
-  /// Có thể truyền bằng --dart-define=SUPABASE_URL=...
   static const String url = String.fromEnvironment(
     'SUPABASE_URL',
-    defaultValue: 'https://hczzslleqblzmrzweoon.supabase.co',
+    defaultValue: '',
   );
 
-  /// ANON/PUBLISHABLE KEY của dự án Supabase.
-  /// Có thể truyền bằng --dart-define=SUPABASE_ANON_KEY=...
   static const String anonKey = String.fromEnvironment(
     'SUPABASE_ANON_KEY',
-    defaultValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjenpzbGxlcWJsem1yendlb29uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjYwOTQsImV4cCI6MjA5NTcwMjA5NH0.fg3SSNSYPjjIw3NOHI-ktEgvABH8ZdKDxOXl9W7Vf34',
+    defaultValue: '',
   );
 }
